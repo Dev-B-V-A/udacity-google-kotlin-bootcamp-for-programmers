6888c67655a464ac58d9a8912378b7bce2c94b28
vladislav.bobylev <bobylev.v.a@yandex.ru>
2018-09-29 11:01:39 +0300

    Lesson 3: Quiz 3: extended task

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,5 +1,18 @@
+import java.util.*
+
 fun dayOfWeek() {
+    val today: Int = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
     println("What day is it today?")
+    when (today) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Not exist day")
+    }
 }
 
 fun main(args: Array<String>) {
