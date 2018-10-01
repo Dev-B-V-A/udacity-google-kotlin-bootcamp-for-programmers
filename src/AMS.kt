80f2916aabba49847557595673baf432aa520160
vladislav.bobylev <bobylev.v.a@yandex.ru>
2018-10-01 21:56:49 +0300

    Lesson 3: Quiz 7: Practice Time

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,8 +1,17 @@
+fun getFortuneCookie(): String {
+    val fortuneList = listOf("You will have a great day!",
+            "Things will go well for you today.",
+            "Enjoy a wonderful day of success.",
+            "Be humble and all will turn out well.",
+            "Today is a good day for exercising restraint.",
+            "Take it easy and enjoy life!",
+            "Treasure your friends because they are your greatest fortune.")
+    print("Enter your birthday:")
+    val birthday = readLine()?.toIntOrNull(10) ?: 1
+    val index = birthday.rem(fortuneList.count())
+    return fortuneList[index]
+}
+
 fun main(args: Array<String>) {
-    val hour = args[0].toInt()
-    when (hour) {
-        in 1..12 -> println("Good morning Kotlin: $hour")
-        in 12..24 -> println("Good night Kotlin: $hour")
-        else -> println("Invalid time")
-    }
+    println("Your fortune: ${getFortuneCookie()}")
 }
