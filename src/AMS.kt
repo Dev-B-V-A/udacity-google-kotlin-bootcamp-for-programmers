ba717fb3056e41019d1fd1ae55f6df484df86450
vladislav.bobylev <bobylev.v.a@yandex.ru>
2018-10-02 21:51:43 +0300

    Lesson 3: Quiz 9: Practice Time

--- src/AMS.kt
+++ src/AMS.kt
@@ -7,9 +7,17 @@ fun getFortuneCookie(): String {
             "Take it easy and enjoy life!",
             "Treasure your friends because they are your greatest fortune.")
     print("Enter your birthday:")
-    val birthday = readLine()?.toIntOrNull(10) ?: 1
-    val index = birthday.rem(fortuneList.count())
-    return fortuneList[index]
+    val birthday = getBirthday()
+    return when (birthday) {
+        28 -> fortuneList[0]
+        31 -> fortuneList[1]
+        in 1..7 -> fortuneList[2]
+        else -> fortuneList[birthday.rem(fortuneList.size)]
+    }
+}
+
+fun getBirthday(): Int {
+    return readLine()?.toIntOrNull(10) ?: 1
 }
 
 fun main(args: Array<String>) {
