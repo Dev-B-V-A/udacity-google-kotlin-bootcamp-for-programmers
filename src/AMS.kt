9b9a9db9fc649c1edeffc554b7b9f98487045805
vladislav.bobylev <bobylev.v.a@yandex.ru>
2018-10-04 21:27:26 +0300

    Lesson 3: Quiz 13: Practice Time

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,24 +1,12 @@
-import com.sun.org.apache.xpath.internal.operations.Bool
-
-fun canAddFish(tankSize: Double,
-               fishes: List<Int>,
-               fishSize: Int = 2,
-               hasDecorations: Boolean = true): Boolean {
-    val allFishesLength = fishes.sum()
-    val multiplier: Double = if (hasDecorations) 0.8 else 1.0
-    val reallyTankSize = tankSize.times(multiplier)
-    return (allFishesLength + fishSize <= reallyTankSize.toInt())
-}
-
-fun unitTest(): Boolean {
-    var testResult = true
-    testResult = testResult && (canAddFish(10.0, listOf(3, 3, 3)) == false)
-    testResult = testResult && (canAddFish(8.0, listOf(2, 2, 2), hasDecorations = false) == true)
-    testResult = testResult && (canAddFish(9.0, listOf(1, 1, 3), 3) == false)
-    testResult = testResult && (canAddFish(10.0, listOf(), 7, true) == true)
-    return testResult
+fun whatShouldIDoToday(mood: String,
+                       weather: String = "sunny",
+                       temperature: Int = 24): String {
+    return when {
+        mood == "happy" && weather == "sunny" && temperature > 20 -> "go for a walk"
+        else -> "stay at home and learning"
+    }
 }
 
 fun main(args: Array<String>) {
-    println(unitTest())
+    println(whatShouldIDoToday("happy"))
 }
