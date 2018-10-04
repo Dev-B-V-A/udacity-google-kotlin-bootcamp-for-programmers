24ae04103a0f49052b5402ff6d205d028cf44570
vladislav.bobylev <bobylev.v.a@yandex.ru>
2018-10-04 22:07:49 +0300

    Lesson 3: Quiz 15: Practice Time

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,12 +1,26 @@
-fun whatShouldIDoToday(mood: String,
-                       weather: String = "sunny",
+import java.awt.GraphicsConfigTemplate
+
+fun canWeGoForAWalk(mood: String,
+                    weather: String,
+                    temperature: Int) = mood == "happy" && weather == "sunny" && temperature > 20
+
+fun goSwimming(temperature: Int) = temperature > 35
+
+fun badWeather(weather: String) = weather == "rainy"
+
+fun whatShouldIDoToday(weather: String = "sunny",
                        temperature: Int = 24): String {
+    val mood = readLine()!!
     return when {
-        mood == "happy" && weather == "sunny" && temperature > 20 -> "go for a walk"
+        canWeGoForAWalk(mood, weather, temperature) -> "go for a walk"
+        goSwimming(temperature) -> "go swimming"
+        badWeather(weather) -> "get umbrella"
         else -> "stay at home and learning"
     }
 }
 
 fun main(args: Array<String>) {
-    println(whatShouldIDoToday("happy"))
+    repeat(3) {
+        println(whatShouldIDoToday())
+    }
 }
