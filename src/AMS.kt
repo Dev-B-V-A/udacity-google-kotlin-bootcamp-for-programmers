7a5a856d94a8ee3bcf77a26b9a1700fa75fdf8bf
vladislav.bobylev <bobylev.v.a@yandex.ru>
2018-10-13 20:42:55 +0300

    Lesson 3: Quiz 17: Practice Time

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,26 +1,7 @@
-import java.awt.GraphicsConfigTemplate
-
-fun canWeGoForAWalk(mood: String,
-                    weather: String,
-                    temperature: Int) = mood == "happy" && weather == "sunny" && temperature > 20
-
-fun goSwimming(temperature: Int) = temperature > 35
-
-fun badWeather(weather: String) = weather == "rainy"
-
-fun whatShouldIDoToday(weather: String = "sunny",
-                       temperature: Int = 24): String {
-    val mood = readLine()!!
-    return when {
-        canWeGoForAWalk(mood, weather, temperature) -> "go for a walk"
-        goSwimming(temperature) -> "go swimming"
-        badWeather(weather) -> "get umbrella"
-        else -> "stay at home and learning"
-    }
-}
-
 fun main(args: Array<String>) {
-    repeat(3) {
-        println(whatShouldIDoToday())
-    }
+    val spices = listOf("curry", "pepper", "cayenne", "ginger", "red curry", "green curry", "red pepper")
+    val currySortedFiltered = spices.filter { it.contains("curry") }.sortedBy { it.length }
+    val curryFiltered = spices.filter { it.startsWith("c") }.filter { it.endsWith("e") }
+    println(currySortedFiltered)
+    println(curryFiltered)
 }
